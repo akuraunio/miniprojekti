c2cfda75f524937c084f29fe1c12d137e34c225b
gabriellakodra <gabriella.kodra@gmail.com>
2025-11-16 18:47:37 +0200

    siirretty deleteä eri paikkaan ja muokattu

--- src/reference_app/ref_app.py
+++ src/reference_app/ref_app.py
@@ -18,5 +18,17 @@ def add_reference():
         return redirect(url_for('index'))
     return render_template('add.html')
 
+@app.route("/delete/<int:ref_id>", methods=["GET", "POST"])
+def delete_reference(ref_id):
+    reference = references[ref_id]
+
+    if request.method == "GET":
+        return render_template('delete.html', reference=reference, ref_id=ref_id)
+    
+    if request.method == "POST":
+        if "delete" in request.form:
+            references.pop(ref_id)
+        return redirect(url_for('index'))
+
 if __name__ == "__main__":
     app.run(debug=True)
