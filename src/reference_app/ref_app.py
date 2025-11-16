26186b32c724d04c566f2b25138d647bbe3dfc68
Unna Mäntyniemi <unna.mantyniemi@helsinki.fi>
2025-11-16 22:48:06 +0200

    Jatkettu edit.html ja ref_app.py

--- src/reference_app/ref_app.py
+++ src/reference_app/ref_app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, redirect, url_for, render_template
+from repositories import references_repository
 
 app = Flask(__name__)
 references = []
@@ -18,9 +19,20 @@ def add_reference():
         return redirect(url_for('index'))
     return render_template('add.html')
 
-@app.route('/edit', methods=['POST'])
-def edit_reference():
-    if request.method == 'POST':
+@app.route('/edit/<int:reference_id>', methods=['POST'])
+def edit_reference(reference_id):
+    reference = references_repository.get_reference(reference_id)
+
+    if not reference:
+        abort(404)
+
+    reference["title"] = request.form["title"]
+    reference["authors"] = request.form["authors"]
+    reference["year"] = request.form["year"]
+    reference["isbn"] = request.form["isbn"]
+    reference["publisher"] = request.form["publisher"]
+
+    return redirect("/reference/" + str(reference_id))
 
 if __name__ == "__main__":
     app.run(debug=True)
