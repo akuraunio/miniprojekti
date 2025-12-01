4073cec10ef31d2e00c0f1d2ae4a4960776b0828
akuraunio <160453163+akuraunio@users.noreply.github.com>
2025-12-01 13:27:00 +0200

    yksikkötesti references_to_bibtex funktiolle

--- src/tests/test_bibtex_transform.py
+++ src/tests/test_bibtex_transform.py
@@ -1,5 +1,5 @@
 import unittest
-from bibtex_transform import reference_to_bibtex
+from bibtex_transform import reference_to_bibtex, references_to_bibtex
 from entities import references
 from reference_data import TestReferenceType, TestReferenceField
 
@@ -20,4 +20,32 @@ class TestTransformReferencesToBibtex(unittest.TestCase):
         self.assertEqual(
             reference_to_bibtex(ref),
             "@test{None,\ntitle = {Raamattu},\nauthor = {Jeesus},\nyear = {1},\n}",
+        )
+
+    def test_get_multiple_references_to_bibtex(self):
+        ref1 = references.Reference(
+            TestReferenceType.TEST,
+            1,
+            {
+                TestReferenceField.TITLE: "Raamattu",
+                TestReferenceField.AUTHOR: "Jeesus",
+                TestReferenceField.YEAR: 1,
+            },
+        )
+
+        ref2 = references.Reference(
+            TestReferenceType.TEST,
+            1,
+            {
+                TestReferenceField.TITLE: "Aapinen",
+                TestReferenceField.AUTHOR: "Mikael Agricola",
+                TestReferenceField.YEAR: 1700,
+            },
+        )
+
+        refs = [ref1, ref2]
+
+        self.assertEqual(
+            references_to_bibtex(refs),
+            "@test{None,\ntitle = {Raamattu},\nauthor = {Jeesus},\nyear = {1},\n}\n\n@test{None,\ntitle = {Aapinen},\nauthor = {Mikael Agricola},\nyear = {1700},\n}",
         )
